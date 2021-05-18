1555fd0805f15bab3bdcea6cea1149b0310c9aa2
germandus <germandouce@gmail.com>
2021-05-18 03:24:50 -0300

    se agregan funciones xa diagrama general

--- Tp_1_Memotest/Memotest.py
+++ Tp_1_Memotest/Memotest.py
@@ -1,9 +1,34 @@
-
 def datos_jugadores():
     pass
 
 
+def proba_cartas():
+    pass
+
+
+def duracion():
+    pass
+
+
 def parametros():
+    duracion()
+    proba_cartas() 
+    pass
+
+
+def crear_tableros():
+    pass
+
+
+def cargar_tableros():
+    pass
+
+
+def nueva_partida():
+    datos_jugadores()
+    parametros()
+    crear_tableros()
+    cargar_tableros()
     pass
 
 
@@ -11,29 +36,82 @@ def score():
     pass
 
 
-def crear_tablero():
-    pass
-
-
-def cargar_tablero():
-    pass
-
-
-def menu():
-    datos_jugadores()
-    parametros()
+def menu_principal():
+    nueva_partida()
     score()
+
+
+def mostrar_tablero():
+    pass
+
+
+def elegir_ficha():
+    pass
+
+
+def pareja_encontrada():
+    pass
+
+
+def hacer_memoria():
+    mostrar_tablero()
+    elegir_ficha()
+    pareja_encontrada()
+    pass
+
+
+def levantar_carta():
+    pass
+
+
+def guardar_carta():
+    pass
+
+
+def carta_replay():
+    pass
+
+
+def carta_layout():
+    pass
+
+
+def carta_toti():
+    pass
+
+
+def carta_fatality():
+    pass
+
+
+def jugar_carta():
+    carta_replay()
+    carta_layout()
+    carta_toti()
+    carta_fatality()
+    pass
+
+
+def ganar():
+    pass
     
 
-def elegir_carta():
+
+def jugando():
+    hacer_memoria()
+    levantar_carta()
+    guardar_carta()
+    jugar_carta()
+    ganar()
+
+
+def guardar_score():
     pass
 
 
 def main():
-    menu()
-    crear_tablero()
-    cargar_tablero()
-    elegir_carta()
-
+    menu_principal()
+    jugando()
+    guardar_score()
 
 main()
