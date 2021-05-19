f7be2bfdfaa205580e73821edae22049310b071f
germandus <germandouce@gmail.com>
2021-05-19 02:26:29 -0300

    Se arregaln PRE Y POSTS  y corrigen otras cosas

--- Tp_1_Memotest/Memotest.py
+++ Tp_1_Memotest/Memotest.py
@@ -1,8 +1,7 @@
 def duracion():
     """
-    ARGS: no recibe argumentos
-    GRAL:defino duracion del juego
-    RETURN: devuelve una variable con la duracion
+    PRE: no recibe argumentos
+    POST: devuelve una variable con la duracion del juego
     """
     print("Defini duracion del juego")
 
@@ -11,18 +10,17 @@ def duracion():
 
 def proba_cartas():
     """
-    ARGS: No recibe argumentos
-    GRAl: Defino proba de cada carta. solo va a devolver 4 valores n una lista, 1 xa ca carta
-    RETURNS: lista con 4 valores de probabilidad
+    PRE: No recibe argumentos
+    POST: Defino proba de cada carta. solo va a devolver 4 valores  de probabilidad
+    n una lista, 1 xa cada carta
     """
     pass
 
 
 def crear_tableros(duracion: int) -> list:
     """
-    GRAL:crea los dos tableros
-    ARGS: la duracion ingresada en parametros()
-    RETURNS: 2 listas de listas con tableros
+    PRE: duarcion es un entero pedido en la funcion duracion()
+    POST: devuelve 2 listas de listas con tableros
     """
     if duracion == 1:
         tablero_1 = []
@@ -35,14 +33,16 @@ def crear_tableros(duracion: int) -> list:
 
 def cargar_tableros():
     """
-    GRAL:carga los dos tableros
-    RETURNS: 2 listas de listas con tableros
+    PRE: No recibe argumentos
+    RETURNS: devuelve 2 listas de listas con los tableros cargados
     """
     pass
 
 
 def nueva_partida():
-    """crea una nueva partida, solo trae el numero de partida como parametro xa mejor refrerencia
+    """
+    PRE: cuando este terminada, va a recibir el numero de partida
+    POST: crea una nueva partida, devolviendo 2 listas con  los tableros creados y cargados
     """
     jug_1 = input ('Ingrese nombre del jugador 1: ')
     jug_2 = input ('Ingrese nombre del jugador 2: ')
@@ -56,14 +56,17 @@ def nueva_partida():
 
 def score():
     """
-    muestra lista vacia si es primera partida, sino muestra scores
+    PRE: por ahora nada...
+    POST: no devuelve nada. solo printea que no se jugaron partidas si es la primera, 
+    sino muestra ultimos 4 scores
     """
     pass
 
 
 def menu_principal():
     """
-    recibe numero de partida, xa el score, si es primera partida muestra lista vacia
+    PRE: numero de partida
+    POST: no devuelve nada, simplemente es un menu de opciones
     """
     salir = False
     while not salir:
@@ -84,58 +87,65 @@ def menu_principal():
         if salir =='1':
             salir = True       
         else:
-            salir = False
+            salir = False 
 
 
 def mostrar_tablero():
     """
-    muestra el tablero
-    """
-    pass
-
-
-def elegir_ficha():
-    """
-    Eleccion de fichas a v er si coinciden
+    PRE:
+    POST: no devuelve nada. Muestra el tablero
     """
     pass
 
 
 def pareja_encontrada():
     """
-    GRAL: devuelve un bool si encontro o no la pareja
-    ARGS: fichas del jugador
+    PRE: fichas del jugador
+    POST: devuelve un bool si encontro o no la pareja
+    
     RETURNS: bool
     """
     pass
 
 
+def elegir_ficha():
+    """
+    PRE:
+    POS: Eleccion de fichas xa ver si coinciden. De aca se deduciran otras funciones xa corrborar
+    si fue correcta la eleccion o no y xa la consiguiente modificacion del tablero.
+    """
+    pareja_encontrada()
+    pass
+
+
 def hacer_memoria():
     """
-    GRAL: muestra tableros y permite jugar. si encontro correcta// (pareja_enconyrada(), corre de nuevo
-    hasta q pierda)
-    ARGS: recibe el tablero del judaor a o b
-    RETURNS: el tablero nuevo segun lo q adivini
+    GRAL: muestra tableros y permite jugar. si encontro correcta// (pareja_enconyrada(), corre de 
+    nuevo hasta q pierda)
+    PRE: recibe el tablero del judaor a o b
+    POST: devuelve el tablero nuevo segun lo q adivinado
     """
     mostrar_tablero()
     elegir_ficha()
-    pareja_encontrada()
     pass
 
 
 def levantar_carta():
     """
-    GRAL: ahora q termino el turno, xq perdio levanta carta. aca se calcula efectivamente q carta toca
-    segun las probas definidas en funcion  parametrso
-    ARGS:  IMPORTANTE, probabilidades de cada carta xa q aqui se haga efectivamente el calculo con random
-    RETURNS: devuelve la carta q toco
+    GRAL: ahora q termino el turno xq perdio, se levanta carta. Aca se calcula efectivamente q carta
+    toca segun las probas definidas en funcion proba_carta()
+    PRE:  IMPORTANTE, probabilidades de cada carta xa q aqui se haga efectivamente el calculo con 
+    random
+    POST: devuelve la carta o no carta q haya tocado
     """
     pass
 
 
 def guardar_carta():
     """
-    GRAL: nada, hacmos una lista xa cada jugador xa q guarde sus cartas
+    PRE: recibe la carta
+    GRAL: hacemos listas xa q cada jugador guarde sus cartas
+    POST: no devuelve nada. solo guarda la carta
     """
     pass
 
@@ -166,10 +176,12 @@ def carta_fatality():
 
 def jugar_carta():
     """
-    Gral: se juega la carta q toco en levantar carta. incluir pocibilidad de jugar mas de una carta
-    de las q se guardaron en guardar carta.
-    ARGS: cartas guardadas de guardar carta
-    Returns: devuelve el tablero cambiado segun la carta, sino, s epierde los cambios ojo
+    si el jugador así lo deseó
+    GRAL: incluir posibilidad de jugar mas de una carta e las q se guardaron en guardar carta.
+    Las funciones se ejecutan segun la carta q se juegue
+    PRE: cartas guardadas de guardar carta y el tablero
+    Returns: en principio, si no entendi mal, no devuelve nada. xq el tablero se modifica x 
+    referencia.
     """
     carta_replay()
     carta_layout()
@@ -190,10 +202,11 @@ def ganar():
 
 def jugando():
     """
-    GRAL:el juego en si
-    ARGS: recibe el numero d ejuego (puede ser util), los tableros, LAS PROBABILIDADES de cada carta
-    damos opcion de jugar carta inmediatamente o guardarla.
-    RETURNS: gano o alguien o no y quien
+    GRAL: el juego en si. Primero se inenta encontrar las cartas iguales. Dsps con otras funciones 
+    se levanta la carta ys juega.
+    PRE: recibe el numero d ejuego (puede ser util), los tableros, LAS PROBABILIDADES de cada carta
+    La carta se guarda predeterminada//. Dsps damos opcion de jugar carta inmediata// o no.
+    POST: devuelve si gano alguien y quien
     """
     hacer_memoria()
     levantar_carta()
@@ -206,16 +219,16 @@ def guardar_score():
     """
     GRAL: cuando alguien gano, se guarda su score. va a haber 4 scores en total. Aqui tengo q crear la
     lista con los scores xa consultar dsps.
-    ARGS: datos jugador
-    RETURNS: nada. solo guarda,
+    PRE: datos jugador
+    POSTS: nada. solo guarda scores
     """
     pass
 
 
 def main() -> None:
     """
-    con un for in range (4) corre xa cada partida
-    defino variable con nuemero de partida ()
+    GRAL: con un for in range (4) corre xa cada partida
+    definir variable con numero de partida (). cuando se acaba guarda el score de dicha partida.
     """
     menu_principal()
     jugando()
