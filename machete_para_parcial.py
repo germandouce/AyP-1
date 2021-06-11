c13e697f43d87cc94136bb3cc325270f3171e1c3
germandus <germandouce@gmail.com>
2021-06-11 16:03:06 -0300

    machete para parcial

---
+++ machete_para_parcial.py
@@ -0,0 +1,17 @@
+#Validador de ocpiones para menu
+#opc = validar (1, 2, 'texto en str con pregunta')
+#Util para menus y para corte de ciclos tipo seguir ingresando( 1- si  0- no)
+
+def validar_opcion(opc_minimas: int, opc_maximas: int, texto: str = '') -> str:
+    """
+    PRE: "opc_minimas" y "opc_maximas" son dos números enteros que 
+    simbolizan la cantidad de opciones posibles.
+
+    POST: Devuelve en formato string la var "opc" con un número 
+    entero dentro del rango de opciones.
+    """
+    opc = input("{}".format(texto))
+    while not opc.isnumeric() or int(opc) > opc_maximas or int(opc) < opc_minimas:
+        opc = input("Por favor, ingrese una opcion valida: ")
+    
+    return opc
