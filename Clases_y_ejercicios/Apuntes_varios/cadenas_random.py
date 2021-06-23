b2b338d229878998c0cb71949b5f3aa2de361879
germandus <germandouce@gmail.com>
2021-06-23 14:27:00 -0300

    apuntes

---
+++ Clases_y_ejercicios/Apuntes_varios/cadenas_random.py
@@ -0,0 +1,23 @@
+import os
+import string
+from random import sample
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+NOMBRE_DEL_ARCHIVO = "cadenas.txt"
+RUTA = os.path.join(BASE_DIR, NOMBRE_DEL_ARCHIVO)
+TAMANIO = 6
+LINEAS = 1000
+
+def escribir_archivo(letras: str) -> None:
+
+    with open(RUTA, 'w') as archivo:
+        for i in range(LINEAS):
+            string_random =  "".join(sample(letras, TAMANIO))      
+            archivo.write(string_random + '\n')
+    
+
+
+def main() -> None:
+    letras = string.ascii_lowercase
+    escribir_archivo(letras)
+main()
